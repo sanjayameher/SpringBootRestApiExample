4d3eae5ad714683c47d287c9fe002556ff370512
sanjayameher <sanjayameher@gmail.com>
2019-10-26 17:50:20 +0530

    given the comments

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,6 +6,7 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Spring boot application which is annotated by @SpringBootApplication
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
